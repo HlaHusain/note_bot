b1f0cab32383692657ee30b931e599e17d5e5bec
hadeelalzenaty-web <hadeelsaleh2279@gmail.com>
2023-06-20 19:24:54 +0200

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -40,7 +40,7 @@ const Signup = ({ setUser }) => {
     },
     heading: {
       fontFamily: 'Poppins',
-      marginBottom: '1.2rem', 
+      marginBottom: '1.2rem',
     },
     input: {
       background: "white"
@@ -51,7 +51,7 @@ const Signup = ({ setUser }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!name || !email) return; //if there is no name or email
+    if (!name || !email || !password) return; //if there is no name or email
     setUser({ name: name, email: email, study: study, password: password });
     console.log(name, email);
     navigate('/dashboard');
@@ -60,97 +60,98 @@ const Signup = ({ setUser }) => {
 
   return (
 
-<Box sx=
-{{display: 'flex',flexDirection:{sm:'column',md:'row'} ,
-justifyContent:'center',
-alignItems: 'center',
-}}>
-      <Box sx={{ maxWidth: '100%', height: 'auto' ,display: {xs:'none',sm:'block'}}}>
+    <Box sx=
+      {{
+        display: 'flex', flexDirection: { sm: 'column', md: 'row' },
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Box sx={{ maxWidth: '100%', height: 'auto', display: { xs: 'none', sm: 'block' } }}>
         <img src={require("../assets/sign-up.gif")} alt="singup" className="singup-img" loading='lazy' />
       </Box>
 
-        <Box className={classes.formContainer}>
-          <Typography variant="h5" className={classes.heading}> Sign Up </Typography>
+      <Box className={classes.formContainer}>
+        <Typography variant="h5" className={classes.heading}> Sign Up </Typography>
 
 
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField  InputProps={{ className: classes.input }}
-              variant='outlined'
-              margin="normal"
-              required
-              fullWidth
-              name="name"
-              label="User Name"
-              type="text"
-              id="name"
-              autoComplete="name"
-              size='small'
-              autoFocus
-              value={name}
-              sx={{ backgroundColor: 'white'}}
-              onChange={(e) => setName(e.target.value)}
-            />
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField InputProps={{ className: classes.input }}
+            variant='outlined'
+            margin="normal"
+            required
+            fullWidth
+            name="name"
+            label="User Name"
+            type="text"
+            id="name"
+            autoComplete="name"
+            size='small'
+            autoFocus
+            value={name}
+            sx={{ backgroundColor: 'white' }}
+            onChange={(e) => setName(e.target.value)}
+          />
 
-            <TextField  InputProps={{ className: classes.input }}
-              variant='outlined'
+          <TextField InputProps={{ className: classes.input }}
+            variant='outlined'
 
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="password"
-              size='small'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="password"
+            size='small'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
 
-            />
+          />
 
-            <TextField  InputProps={{ className: classes.input }}
-              variant='outlined'
-              margin="normal"
-              required
-              fullWidth
-              name="email"
-              label="Email Address"
-              type="text"
-              id="mail"
-              size='small'
-              autoComplete="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+          <TextField InputProps={{ className: classes.input }}
+            variant='outlined'
+            margin="normal"
+            required
+            fullWidth
+            name="email"
+            label="Email Address"
+            type="text"
+            id="mail"
+            size='small'
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
 
-            />
-            <TextField  InputProps={{ className: classes.input }}
-              variant='outlined'
-              margin="normal"
-              required
-              fullWidth
-              name="study"
-              label="Study Field"
-              type="text"
-              id="study"
-              size='small'
-              autoComplete="your study field"
-              value={study}
-              onChange={(e) => setStudy(e.target.value)}
+          />
+          <TextField InputProps={{ className: classes.input }}
+            variant='outlined'
+            margin="normal"
+            required
+            fullWidth
+            name="study"
+            label="Study Field"
+            type="text"
+            id="study"
+            size='small'
+            autoComplete="your study field"
+            value={study}
+            onChange={(e) => setStudy(e.target.value)}
 
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              className={classes.root}
-            >
-              Sign up
-            </Button>
-          </Box>
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            className={classes.root}
+          >
+            Sign up
+          </Button>
         </Box>
-
       </Box>
+
+    </Box>
   );
 };
 export default Signup;
