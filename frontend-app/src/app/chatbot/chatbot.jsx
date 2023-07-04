a70b68771c99170b5b622778f723859e36d75b41
hadeelalzenaty-web <hadeelsaleh2279@gmail.com>
2023-07-04 23:30:41 +0200

    Update chatbot.jsx

--- frontend-app/src/app/chatbot/chatbot.jsx
+++ frontend-app/src/app/chatbot/chatbot.jsx
@@ -9,7 +9,7 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 export const ChatBot = () => {
   return (
     <Box sx={{ height: "100%",
-     width:388,border:"1px solid #C1C1C1", borderRadius: 4,
+     width:360,border:"1px solid #C1C1C1", borderRadius: 4,
      margin:"0 auto"}}>
       <Box sx={{ flex: 1, overflowY: "auto", p: 2,maxHeight:"400px", height:"100%"}}>
         <Grid
