5c8dd0e28650414ef93f28c3a18f6ec1a5584cac
Yusra Abdulrahman <yusraabdulrahman@yusras-mbp.localdomain>
2023-07-05 10:15:06 +0200

    trying to solve chatbot apis

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,60 +1,84 @@
-const { Configuration, OpenAIApi } = require("openai");
-const OPENAI_KEY = "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";
+const { Configuration, OpenAIApi, RateLimitError} = require("openai");
+//const backoff = require('backoff');
+// const HttpError = require("../model/http-error");
 
 const openAIConfig = new Configuration({
-  apiKey: OPENAI_KEY
+    organization: "org-65qL0uV70AbvbEVHEZcr9iD8",
+    apiKey: "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",
 });
 
 const openapi = new OpenAIApi(openAIConfig);
 
-// Define a function to handle chat completions and manage rate limits
-const chatCompletion = async (req, res) => {
-  try {
-    const { prompt } = req.body;
-
-    const answer = await makeAPIRequestWithRateLimit(prompt);
-
-    const text = answer.data.choices[0].text;
-
-    res.status(200).json({ text });
-
-   } catch (err) {
-     res.status(500).json({
-       message: err.message
-     });
-   }
-};
-
-const makeAPIRequestWithRateLimit = async (prompt) => {
-    const maxRetries = 3; // Maximum number of retries before giving up
-    let retryDelay = 1000; // Initial retry delay in milliseconds (1 second)
-  
-    for (let retry = 0; retry < maxRetries; retry++) {
-      try {
-        const answer = await openapi.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0, // Higher values means the model will take more risks.
-            max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-            top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-            frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-            presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+const chatCompletion = async (req, res, next) => {
+    try {
+        const prompt = req.body.prompt;
+    
+        const response = await openapi.createCompletion({
+          model: "text-davinci-003",
+          prompt: `${prompt}`,
+          temperature: 0, // Higher values means the model will take more risks.
+          max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+          top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+          frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+          presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
         });
-  
-        return answer; // Return the response if successful
+    
+        res.status(200).send({
+          bot: response.data.choices[0].text
+        });
+    
       } catch (error) {
-        if (error.response && error.response.status === 429) {
-          // Rate limit reached, wait for the specified time and then retry
-          await wait(retryDelay);
-          retryDelay *= 2; // Increase the retry delay exponentially with each retry
-        } else {
-          throw error; // Throw the error if it's not a rate limit issue
-        }
+        console.error(error)
+        res.status(500).send(error || 'Something went wrong');
       }
-    }
-    throw new Error("Rate limit exceeded, and maximum retries reached.");
-  };
+    };
+    
+// const chatCompletion = async (req, res) => {
+//     try {
+//       const { prompt } = req.body;
   
-  const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+//       const answer = await makeAPIRequestWithRateLimit(prompt);
   
-exports.chatCompletion = chatCompletion;
+//       const text = answer.choices[0].text;
+  
+//       res.status(200).json({ text });
+//     } catch (err) {
+//       res.status(500).json({
+//         message: err.message,
+//       });
+//     }
+//   };
+  
+//   const makeAPIRequestWithRateLimit = async (prompt) => {
+//     const maxRetries = 5;
+//     let retryDelay = 1000;
+  
+//     for (let retry = 0; retry < maxRetries; retry++) {
+//       try {
+//         const answer = await openapi.createCompletion({
+//           model: "text-davinci-003",
+//           prompt: `${prompt}`,
+//           temperature: 0,
+//           max_tokens: 3000,
+//           top_p: 1,
+//           frequency_penalty: 0.5,
+//           presence_penalty: 0,
+//         });
+  
+//         return answer;
+//       } catch (error) {
+//         if (error.response && error.response.status === 429) {
+//           await wait(retryDelay);
+//           retryDelay *= 2;
+//         } else {
+//           throw error;
+//         }
+//       }
+//     }
+//     throw new Error("Rate limit exceeded, and maximum retries reached.");
+//   };
+  
+//   const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  
+  exports.chatCompletion = chatCompletion;
--- frontend-app/src/app/courses/routes.jsx
+++ frontend-app/src/app/courses/routes.jsx
@@ -3,23 +3,6 @@ import { CoursesList } from "./list";
 import { SearchByCourse } from "./search";
 
 
-const courses = [
-  { id: 1, title: 'Advanced Web Tech', notes: [
-    { id: 1, title: 'Note 1', content: 'Lorem ipsum dolor sit amet' },
-    { id: 2, title: 'Note 2', content: 'Consectetur adipiscing elit' },
-  ] },
-  { id: 2, title: 'Interactive Systems', notes: [
-    { id: 3, title: 'Note 3', content: 'Pellentesque ac ligula' },
-    { id: 4, title: 'Note 4', content: 'Mauris aliquet rhoncus est' },
-    { id: 5, title: 'Note 5', content: 'Nullam congue libero' },
-  ] },
-  // Add more courses as needed
-];
-const handleDeleteCourse = courseId => {
-  // Handle the delete course logic
-};
-
-
 export const routes = [
   {
     path: "/courses",
